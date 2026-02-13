0064b9e0c089347a34761659e2860aa30cd6e83d
Deep Kamani <deep.kamani@prominentpixel.com>
2026-02-13 17:20:29 +0530

    Added stopPropogation() method to prevent event bubbling.

--- Practice_OOP/assets/scripts/events.js
+++ Practice_OOP/assets/scripts/events.js
@@ -1,9 +1,9 @@
-const buttons=document.querySelectorAll('button');
+const button=document.querySelector('button');
 
-const buttonClickHandler = event => {
-    event.target.disabled=true;
-    console.log(event);
-};
+// const buttonClickHandler = event => {
+//     event.target.disabled=true;
+//     console.log(event);
+// };
 
 // button.addEventListener('click',buttonClickHandler);
 
@@ -16,5 +16,17 @@ const buttonClickHandler = event => {
 const form=document.querySelector('form');
 form.addEventListener('click',event=>{
     event.preventDefault();
+    console.log(event);
+});
+
+const div=document.querySelector('div');
+div.addEventListener('click',(event)=>{
+    console.log("DIV CLICKED");
+    console.log(event);
+});
+
+button.addEventListener('click',(event)=>{
+    event.stopPropagation();
+    console.log("BUTTON CLICKED");
     console.log(event);
 });
