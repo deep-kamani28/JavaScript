14680cdd24562ca20091ca9a37be7beb93b798d3
Deep Kamani <deep.kamani@prominentpixel.com>
2026-02-14 16:16:09 +0530

    Start learning async.

--- Functions/app.js
+++ Functions/app.js
@@ -1,40 +1,50 @@
-const startGameBtn = document.getElementById('start-game-btn'); 
+const startGameBtn = document.getElementById("start-game-btn");
 
-const ROCK = 'ROCK';
-const PAPER = 'PAPER';
-const SCISSORS = 'SCISSORS';
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
-const RESULT_DRAW='DRAW';
-const RESULT_PLAYER_WINNER='PLAYER_WINS';
-const RESULT_COMPUTER_WINNER='COMPUTER_WINS';
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINNER = "PLAYER_WINS";
+const RESULT_COMPUTER_WINNER = "COMPUTER_WINS";
 
-const getPlayerChoice = function() {
-  const selection=prompt(`${ROCK},${PAPER} or ${SCISSORS} ?`,'').toUpperCase();
-  if(selection!==ROCK && selection!==PAPER && selection!==SCISSORS){
-    alert('Invalid User Input');
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK},${PAPER} or ${SCISSORS} ?`,
+    "",
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert("Invalid User Input");
     return DEFAULT_USER_CHOICE;
   }
   return selection;
 };
 
-const getWinner=(cChoice,pChoice) => cChoice===pChoice ? RESULT_DRAW : cChoice===ROCK && pChoice===PAPER || cChoice===PAPER && pChoice===SCISSORS || cChoice===SCISSORS && pChoice===ROCK ? RESULT_PLAYER_WINNER : RESULT_COMPUTER_WINNER;
-const getComputerChoice=function() {
-  const randomValue=Math.random();
-  if(randomValue<0.34){
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+        (cChoice === PAPER && pChoice === SCISSORS) ||
+        (cChoice === SCISSORS && pChoice === ROCK)
+      ? RESULT_PLAYER_WINNER
+      : RESULT_COMPUTER_WINNER;
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
     return ROCK;
-  }else if(randomValue<0.67){
+  } else if (randomValue < 0.67) {
     return PAPER;
-  }else{
+  } else {
     return SCISSORS;
   }
 };
 
-startGameBtn.addEventListener('click', function() {
+startGameBtn.addEventListener("click", function () {
   console.log("Game is starting...");
-  const playerSelection=getPlayerChoice();
-  const computerSelection=getComputerChoice();
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
   console.log(playerSelection);
   console.log(computerSelection);
-  const winner=getWinner(computerSelection,playerSelection);
+  const winner = getWinner(computerSelection, playerSelection);
   console.log(winner);
 });
