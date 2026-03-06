cbc5c8d36a5f7b64a8e9b99b41f2ad01220dbdb8
Deep Kamani <deep.kamani@prominentpixel.com>
2026-03-06 17:28:48 +0530

    fetch data ysing async await.

--- HTTP_Request/assets/scripts/app.js
+++ HTTP_Request/assets/scripts/app.js
@@ -1,3 +1,41 @@
+// const listElement=document.querySelector('.posts');
+// const postTemplate=document.getElementById('single-post');
+
+// function sendHttpRequest(method,url){
+//     const promise=new Promise((resolve,reject)=>{
+//         const xhr=new XMLHttpRequest();
+
+//         xhr.open(method,url);
+
+//         xhr.responseType='json';
+
+//         xhr.onload=function (){
+//             resolve(xhr.response);
+//             // const listOfPost=JSON.parse(xhr.response);
+            
+//         }
+//         xhr.send();
+//     });
+//     return promise;
+// }
+
+// function fetchPost(){
+//     sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts').then(
+//         responseData=>{
+//             const listOfPost=responseData;
+//             for(const post of listOfPost){
+//                 const postEl=document.importNode(postTemplate.content,true);
+//                 postEl.querySelector('h2').textContent=post.title.toUpperCase();
+//                 postEl.querySelector('p').textContent=post.body;
+//                 listElement.append(postEl);
+//             }
+            
+//         }
+//     );
+// }
+
+// fetchPost();
+
 const listElement=document.querySelector('.posts');
 const postTemplate=document.getElementById('single-post');
 
@@ -19,19 +57,15 @@ function sendHttpRequest(method,url){
     return promise;
 }
 
-function fetchPost(){
-    sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts').then(
-        responseData=>{
-            const listOfPost=responseData;
-            for(const post of listOfPost){
-                const postEl=document.importNode(postTemplate.content,true);
-                postEl.querySelector('h2').textContent=post.title.toUpperCase();
-                postEl.querySelector('p').textContent=post.body;
-                listElement.append(postEl);
-            }
-            
-        }
-    );
+async function fetchPost(){
+    const responseData=await sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts')
+    const listOfPost=responseData;
+    for(const post of listOfPost){
+        const postEl=document.importNode(postTemplate.content,true);
+        postEl.querySelector('h2').textContent=post.title.toUpperCase();
+        postEl.querySelector('p').textContent=post.body;
+        listElement.append(postEl);
+    }
 }
 
 fetchPost();
