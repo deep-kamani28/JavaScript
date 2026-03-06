966012bdc7f85b8421f121ef25c3b41f48b3c667
Deep Kamani <deep.kamani@prominentpixel.com>
2026-03-06 19:25:33 +0530

    Make delete button clickable.

--- HTTP_Request/assets/scripts/app.js
+++ HTTP_Request/assets/scripts/app.js
@@ -40,6 +40,7 @@ const listElement=document.querySelector('.posts');
 const postTemplate=document.getElementById('single-post');
 const form=document.querySelector('#new-post form');
 const fetchButton=document.querySelector('#available-posts button');
+const postList=document.querySelector('ul');
 
 function sendHttpRequest(method,url,data){
     const promise=new Promise((resolve,reject)=>{
@@ -66,6 +67,7 @@ async function fetchPost(){
         const postEl=document.importNode(postTemplate.content,true);
         postEl.querySelector('h2').textContent=post.title.toUpperCase();
         postEl.querySelector('p').textContent=post.body;
+        postEl.querySelector('li').id=post.id;
         listElement.append(postEl);
     }
 }
@@ -89,3 +91,10 @@ form.addEventListener('submit',event=>{
 
     createPost(enteredTitle,enteredContent);
 });
+
+postList.addEventListener('click', event=>{
+    if(event.target.tagName==='BUTTON'){
+        const postId=event.target.closest('li').id;
+        sendHttpRequest('DELETE',`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    }
+})
