6e086675631da0d2ad772b70724080be70d3b7b5
Deep Kamani <deep.kamani@prominentpixel.com>
2026-03-13 19:23:53 +0530

    Worked on Share_My_Place app.

--- Share_My_Place/src/SharePlace.js
+++ Share_My_Place/src/SharePlace.js
@@ -1,30 +1,67 @@
+import {Modal} from './UI/Modal.js';
+import {Map} from './UI/Map.js';
+import {getcoordfromAddress, getAddressFromCoords} from './Utility/Location.js';
+
 class PlaceFinder{
     constructor(){
         const addressForm=document.querySelector('form');
-        const locateUsreBtn=document.querySelector('locate-btn');
-
-        locateUsreBtn.addEventListener('click', this.locateUserHandler);
-        addressForm.addEventListener('submit', this.findAddressHandler);
+        const locateUsreBtn=document.getElementById('locate-btn');
+        
+        locateUsreBtn.addEventListener('click', this.locateUserHandler.bind(this));
+        addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
     }
 
-    locateUserHandler(){
+    selectPlace(coordinates,address){
+        if(this.map){
+            this.map.render(coordinates);
+        }else{
+            this.map=new Map(coordinates);
+        }
+        
+        
+    }
+
+    async locateUserHandler(){
         if(!navigator.geolocation){
             alert('Location feature is not available in your browser.');
             return;
         }
-        navigator.geolocation.getCurrentPosition(successResult=>{
+
+        const modal=new Modal('loading-modal-content',"Getting your location - Please wait");
+        modal.show();
+        navigator.geolocation.getCurrentPosition(async successResult=>{
+            
             const coordinate={
                 lat:successResult.coords.latitude,
                 lng:successResult.coords.longitude
             };
-            console.log(coordinate);
+            const address=await getAddressFromCoords(coordinate);
+            modal.hide();
+            // console.log(coordinate);
+            this.selectPlace(coordinate,address);
         },error=>{
+            modal.hide();
             alert('Could not locate you.');
         });
     }
 
-    findAddressHandler(){
+    async findAddressHandler(event){
+        event.preventDefault();
+        const address=event.target.querySelector('input');
+        if(!address || address.trim().length===0){
+            alert('Invalid address');
+            return;
+        }
 
+        const modal=new Modal('loading-modal-content',"Getting your location - Please wait");
+        modal.show();
+        try{
+            const coordinates=await getcoordfromAddress(address);
+            this.selectPlace(coordinates,address);
+        }catch(err){
+            alert(err.message);
+        }
+        modal.hide();
     }
 }
 
